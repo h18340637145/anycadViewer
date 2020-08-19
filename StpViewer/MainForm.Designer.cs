189c73baf565124d79e0e89c67679fac6e603efd
hhw <1332073819@qq.com>
2020-08-19 21:32:36 +0800

    add viewer

---
+++ StpViewer/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace StpViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeViewStp = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openSTLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveSTEPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveSTLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sharedAndFrameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.shadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edgeModeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stdViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frontToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.leftToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(6);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeViewStp);
+            this.splitContainer1.Panel1.Controls.Add(this.menuStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(1434, 988);
+            this.splitContainer1.SplitterDistance = 478;
+            this.splitContainer1.SplitterWidth = 8;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeViewStp
+            // 
+            this.treeViewStp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewStp.ForeColor = System.Drawing.Color.YellowGreen;
+            this.treeViewStp.Location = new System.Drawing.Point(0, 39);
+            this.treeViewStp.Margin = new System.Windows.Forms.Padding(6);
+            this.treeViewStp.Name = "treeViewStp";
+            this.treeViewStp.Size = new System.Drawing.Size(478, 949);
+            this.treeViewStp.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(478, 39);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.openSTLToolStripMenuItem,
+            this.saveSTEPToolStripMenuItem,
+            this.saveSTLToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(73, 35);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(274, 44);
+            this.openToolStripMenuItem.Text = "Open STEP";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openSTLToolStripMenuItem
+            // 
+            this.openSTLToolStripMenuItem.Name = "openSTLToolStripMenuItem";
+            this.openSTLToolStripMenuItem.Size = new System.Drawing.Size(274, 44);
+            this.openSTLToolStripMenuItem.Text = "Open STL";
+            this.openSTLToolStripMenuItem.Click += new System.EventHandler(this.openSTLToolStripMenuItem_Click);
+            // 
+            // saveSTEPToolStripMenuItem
+            // 
+            this.saveSTEPToolStripMenuItem.Name = "saveSTEPToolStripMenuItem";
+            this.saveSTEPToolStripMenuItem.Size = new System.Drawing.Size(274, 44);
+            this.saveSTEPToolStripMenuItem.Text = "Save STEP";
+            this.saveSTEPToolStripMenuItem.Click += new System.EventHandler(this.saveSTEPToolStripMenuItem_Click);
+            // 
+            // saveSTLToolStripMenuItem
+            // 
+            this.saveSTLToolStripMenuItem.Name = "saveSTLToolStripMenuItem";
+            this.saveSTLToolStripMenuItem.Size = new System.Drawing.Size(274, 44);
+            this.saveSTLToolStripMenuItem.Text = "Save STL";
+            this.saveSTLToolStripMenuItem.Click += new System.EventHandler(this.saveSTLToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sharedAndFrameToolStripMenuItem,
+            this.shadeToolStripMenuItem,
+            this.edgeModeToolStripMenuItem,
+            this.stdViewToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(89, 35);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // sharedAndFrameToolStripMenuItem
+            // 
+            this.sharedAndFrameToolStripMenuItem.Name = "sharedAndFrameToolStripMenuItem";
+            this.sharedAndFrameToolStripMenuItem.Size = new System.Drawing.Size(396, 44);
+            this.sharedAndFrameToolStripMenuItem.Text = "ShadeWithEdgeMode";
+            this.sharedAndFrameToolStripMenuItem.Click += new System.EventHandler(this.sharedAndFrameToolStripMenuItem_Click);
+            // 
+            // shadeToolStripMenuItem
+            // 
+            this.shadeToolStripMenuItem.Name = "shadeToolStripMenuItem";
+            this.shadeToolStripMenuItem.Size = new System.Drawing.Size(396, 44);
+            this.shadeToolStripMenuItem.Text = "Shade";
+            this.shadeToolStripMenuItem.Click += new System.EventHandler(this.shadeToolStripMenuItem_Click);
+            // 
+            // edgeModeToolStripMenuItem
+            // 
+            this.edgeModeToolStripMenuItem.Name = "edgeModeToolStripMenuItem";
+            this.edgeModeToolStripMenuItem.Size = new System.Drawing.Size(396, 44);
+            this.edgeModeToolStripMenuItem.Text = "EdgeMode";
+            this.edgeModeToolStripMenuItem.Click += new System.EventHandler(this.edgeModeToolStripMenuItem_Click);
+            // 
+            // stdViewToolStripMenuItem
+            // 
+            this.stdViewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topToolStripMenuItem,
+            this.frontToolStripMenuItem,
+            this.leftToolStripMenuItem});
+            this.stdViewToolStripMenuItem.Name = "stdViewToolStripMenuItem";
+            this.stdViewToolStripMenuItem.Size = new System.Drawing.Size(396, 44);
+            this.stdViewToolStripMenuItem.Text = "Std View";
+            // 
+            // topToolStripMenuItem
+            // 
+            this.topToolStripMenuItem.Name = "topToolStripMenuItem";
+            this.topToolStripMenuItem.Size = new System.Drawing.Size(208, 44);
+            this.topToolStripMenuItem.Text = "Top";
+            this.topToolStripMenuItem.Click += new System.EventHandler(this.topToolStripMenuItem_Click);
+            // 
+            // frontToolStripMenuItem
+            // 
+            this.frontToolStripMenuItem.Name = "frontToolStripMenuItem";
+            this.frontToolStripMenuItem.Size = new System.Drawing.Size(208, 44);
+            this.frontToolStripMenuItem.Text = "Front";
+            this.frontToolStripMenuItem.Click += new System.EventHandler(this.frontToolStripMenuItem_Click);
+            // 
+            // leftToolStripMenuItem
+            // 
+            this.leftToolStripMenuItem.Name = "leftToolStripMenuItem";
+            this.leftToolStripMenuItem.Size = new System.Drawing.Size(208, 44);
+            this.leftToolStripMenuItem.Text = "Left";
+            this.leftToolStripMenuItem.Click += new System.EventHandler(this.leftToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(92, 35);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1434, 988);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MainForm";
+            this.Text = "stl & step viewer produced by hhw & framework ";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeViewStp;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openSTLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sharedAndFrameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem shadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edgeModeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stdViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frontToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem leftToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveSTEPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveSTLToolStripMenuItem;
+    }
+}
+
---
+++ StpViewer/MainForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AnyCAD.Platform;
+using AnyCAD.Presentation;
+
+namespace StpViewer
+{
+    public partial class MainForm : Form
+    {
+        private AnyCAD.Presentation.RenderWindow3d renderView = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            var container = this.splitContainer1.Panel2;//new 一个容器
+            renderView = new RenderWindow3d();
+
+            //界面固定代码，写上就完了
+            this.renderView.Dock = DockStyle.Fill;
+            this.renderView.TabIndex = 1;
+            this.splitContainer1.Panel2.Controls.Add(this.renderView);
+            //用鼠标单击控件时发生。
+            this.renderView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnRenderWindow_MouseClick);
+        }
+
+        private void OnRenderWindow_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+        //生成默认渲染视图
+//----------------------------------------------------------------------------------------------------------------------------
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.renderView.ShowCoordinateAxis(true);
+            renderView.ExecuteCommand("ShadeWithEdgeMode");
+            this.renderView.RequestDraw();
+
+            renderView.View3d.ShowCoordinateAxis(false);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------
+        //打开文件功能      
+        //-----------------------------------------------------------------------------------------------------------------------------
+        //打开step文件
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "STEP File(*.stp;*.step)|*.stp;*.step|All Files(*.*)|*.*";
+
+            if (DialogResult.OK == dlg.ShowDialog())
+            {
+                CADBrower browser = new CADBrower(dlg.SafeFileName, this.treeViewStp, this.renderView);
+                AnyCAD.Exchange.StepReader reader = new AnyCAD.Exchange.StepReader();
+                reader.Read(dlg.FileName, browser);
+            }
+            renderView.FitAll();
+        }
+
+        //打开stl文件
+        private void openSTLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "STEP File(*.stl)|*.stl|All Files(*.*)|*.*";
+
+            if (DialogResult.OK != dlg.ShowDialog())
+                return;
+
+            TopoShape shape = GlobalInstance.BrepTools.LoadFile(new AnyCAD.Platform.Path(dlg.FileName));
+            renderView.RenderTimer.Enabled = false;
+            if (shape != null)
+            {
+                //加载到stl文件时
+                TopoShapeGroup group = new TopoShapeGroup();
+                group.Add(shape);
+                
+                SceneManager sceneMgr = renderView.SceneManager;
+                SceneNode rootNode = GlobalInstance.TopoShapeConvert.ToSceneNode(shape, 0.1f);
+                if (rootNode != null)
+                {
+                    sceneMgr.AddNode(rootNode);
+                }
+            }
+            renderView.RenderTimer.Enabled = true;
+            renderView.FitAll();
+            renderView.RequestDraw(EnumRenderHint.RH_LoadScene);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------
+
+
+
+        //清除表面模型
+        //------------------------------------------------------------------------------------------------------------------
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.ClearScene();
+        }
+        //------------------------------------------------------------------------------------------------------------------
+
+
+
+        //三种视图
+        //------------------------------------------------------------------------------------------------------------------
+        //渲染线框
+        private void sharedAndFrameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.SetDisplayMode((int)(EnumDisplayStyle.DS_Face | EnumDisplayStyle.DS_Edge | EnumDisplayStyle.DS_Realistic));
+        }
+
+        //渲染
+        private void shadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.SetDisplayMode((int)(EnumDisplayStyle.DS_Face | EnumDisplayStyle.DS_Realistic));
+        }
+
+        //线框
+        private void edgeModeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.Renderer.SetDisplayMode((int)EnumDisplayStyle.DS_Edge);
+            renderView.RequestDraw();
+        }
+
+        //------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------
+
+        //三视图功能
+        //俯视图
+        private void topToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.Renderer.SetStandardView(EnumStandardView.SV_Top);
+            renderView.RequestDraw();
+        }
+        //主视图
+        private void frontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.Renderer.SetStandardView(EnumStandardView.SV_Front);
+            renderView.RequestDraw();
+        }
+        //左视图
+        private void leftToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            renderView.Renderer.SetStandardView(EnumStandardView.SV_Left);
+            renderView.RequestDraw();
+        }
+
+        //------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------
+        //保存step
+        private void saveSTEPToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "step（*.step）|*.stp";
+            sfd.FilterIndex = 1;
+            sfd.RestoreDirectory = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(sfd.FileName.ToString());
+            }
+            else
+            {
+                MessageBox.Show("取消保存");
+                return;
+            }
+        }
+
+        private void saveSTLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "stl（*.stl）|*.stl";
+            sfd.FilterIndex = 1;
+            sfd.RestoreDirectory = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(sfd.FileName.ToString());
+            }
+            else
+            {
+                MessageBox.Show("取消保存");
+                return;
+            }
+        }
+    }
+}
